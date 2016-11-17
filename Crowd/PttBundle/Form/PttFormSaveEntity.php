3c7c6dab6daf2d43307a906252e16e1f819a595e
Pau Arjona <pau15.1991@gmail.com>
2016-11-17 13:20:41 +0100

    esborrar comentaris

--- Crowd/PttBundle/Form/PttFormSaveEntity.php
+++ Crowd/PttBundle/Form/PttFormSaveEntity.php
@@ -14,28 +14,6 @@ class PttFormSaveEntity extends PttFormSave
     public function value()
     {
     	if(isset($this->sentData[$this->field->name])){
-    		// $entities = $this->entityInfo->get($this->field->name);
-    		// if($entities){
-    		// 	$entitiesArr = $entities->toArray();
-    		// 	foreach ($this->sentData[$this->field->name] as $key => $value) {
-	    	// 		if (isset($value['id'])) {
-	    	// 			$entity = 0;
-	    	// 			foreach ($entitiesArr as $row => $val) {
-	    	// 				if($val->getPttId() == $value['id']){
-	    	// 					$entity = $val;
-	    	// 				}
-	    	// 			}
-
-	    	// 			foreach ($value as $meth => $setter) {
-	    	// 				if($meth != 'id'){
-	    	// 					$method = 'set' . ucfirst($meth);
-	    	// 					$entity->$method($setter);
-	    	// 				}
-	    	// 			}
-	    	// 			$this->sentData[$this->field->name][$key] = $entity;
-	    	// 		}
-	    	// 	}
-    		// }
     		return ($this->languageCode) ? $this->sentData[$this->languageCode][$this->field->name] : $this->sentData[$this->field->name];
     	} else {
     		return [];
