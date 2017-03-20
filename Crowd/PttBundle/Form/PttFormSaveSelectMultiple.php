8c3c02b352bcfb7894eb0bb1d6b41789d686bb33
Pau Arjona Reverter <pau@crowd-studio.com>
2017-03-20 16:30:37 +0100

    s

--- Crowd/PttBundle/Form/PttFormSaveSelectMultiple.php
+++ Crowd/PttBundle/Form/PttFormSaveSelectMultiple.php
@@ -20,17 +20,17 @@ class PttFormSaveSelectMultiple extends PttFormSave
         $id = $this->_sentValue(0);
 
         if ($this->languageCode) {
-            $model = $this->sentData[$this->languageCode][$this->field->name . '_model'];
+            $model = $this->sentData[$this->languageCode][$this->field['name'] . '_model'];
             $entityModel = $pttServices->getSimpleFilter($model . 'Trans', ['where' => ['relatedid' => $id, 'language' => $this->languageCode]]);
             $entityModel = (isset($entityModel[0])) ? $entityModel[0] : null;
         } else {
-            $model = $this->sentData[$this->field->name . '_model'];
+            $model = $this->sentData[$this->field['name'] . '_model'];
             $entityModel = $pttServices->getOne($model, $id);
         }
 
         if ($entityModel) {
             $title = (method_exists($entityModel, 'getTitle')) ? $entityModel->getTitle() : '';
-            $this->entityInfo->set($this->field->name . '_title', $title, $this->languageCode);
+            $this->entityInfo->set($this->field['name'] . '_title', $title, $this->languageCode);
         }
 
         return $this->_value();
