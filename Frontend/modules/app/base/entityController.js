983c6892c64ca6fdf12dfd94648036cb2045dc49
Lluís Marfany <lluis@crowd.st>
2017-03-29 17:57:58 +0200

    real commit (not only package)

--- Frontend/modules/app/base/entityController.js
+++ Frontend/modules/app/base/entityController.js
@@ -1,9 +1,13 @@
 'use strict';
 
 var Sortable = require('sortablejs');
+require('angular-sanitize');
 
 module.exports = /*@ngInject*/
-function entityController($scope, $element, $http, loader) {
+function entityController($scope, $element, $http, loader, $sce) {
+
+	$scope.items = JSON.parse($element[0].querySelector('.entity-block').getAttribute('items'));
+	$scope.templates = JSON.parse($element[0].querySelector('.entity-block').getAttribute('templates'));
 
 	$scope.sortable = Sortable.create($element[0].querySelector('.items-to-order'),{
 		disabled: !$scope.changeOrder,
@@ -34,28 +38,29 @@ function entityController($scope, $element, $http, loader) {
 		$scope.sortable.option("disabled", !$scope.changeOrder);
 	}
 
-	
-
-	$scope.newEvent = function(){
-		console.log("NEW");
+	$scope.newEvent = function(){$scope.new();}
+	$scope.new = function (){
+		$scope.items.push($scope.selectedTemplate);
 	}
 
-	$scope.toggleEntityEvent = function(e,id) {e.preventDefault();$scope.toggleEntity(id)}
-	$scope.toggleEntity = function(id){
-		angular.forEach($element[0].querySelectorAll('[data-id]'), function(value, key) {
-			if(id == value.getAttribute('data-id')){
-				let entity = angular.element(value);
-				if(entity.hasClass('opened')){
-					angular.element(value).removeClass('opened');
-				}else{
-					angular.element(value).addClass('opened');
-				}
-			}
-		});
+	$scope.toggleEntityEvent = function(e,index) {e.preventDefault();$scope.toggleEntity(index)}
+	$scope.toggleEntity = function(index){
+		$scope.items[index].opened = $scope.items[index].opened == undefined ? true : !$scope.items[index].opened;
+	}
+
+	$scope.deleteEntityEvent = function(e,index) {
+		e.preventDefault();
+		if(confirm(window.conf.trans['sureDelete'] )){
+			$scope.deleteEntity(index);
+		}
+	}
+	$scope.deleteEntity = function(index){
+		$scope.items.splice(index, 1);
 	}
 	
 
-	
+	$scope.renderHtml = function(html_code){return $sce.trustAsHtml(html_code);};
+
 
 	$scope.setSortable(false);
 
