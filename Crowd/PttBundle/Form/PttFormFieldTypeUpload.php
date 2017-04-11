bad548ea6294824f343a0456552c148f7ece2f46
Pau Arjona Reverter <pau@crowd-studio.com>
2017-04-11 12:42:02 +0200

    upload

--- Crowd/PttBundle/Form/PttFormFieldTypeUpload.php
+++ Crowd/PttBundle/Form/PttFormFieldTypeUpload.php
@@ -11,57 +11,57 @@ use Crowd\PttBundle\Util\PttUtil;
 
 class PttFormFieldTypeUpload extends PttFormFieldType
 {
-	public function field()
-	{
-		$s3 = PttUtil::pttConfiguration('s3');
+    public function field()
+    {
+        $s3 = PttUtil::pttConfiguration('s3');
 
-		$prefix = $s3['prodUrl'] . $this->entityInfo->getEntityName() . '-' . $this->field->name . '/';
-		$name = ($this->value && $this->value != '') ? $prefix . $this->value : '';
+        $prefix = $s3['prodUrl'] . strtolower($this->entityInfo->getEntityName() . '-' . $this->field->name) . '/';
+        $name = ($this->value && $this->value != '') ? $prefix . $this->value : '';
 
-		$download = ($name != '') ? $name : $prefix;
+        $download = ($name != '') ? $name : $prefix;
 
-		$html = $this->start();
+        $html = $this->start();
 
-		//content here
-		$html .= $this->label();
+        //content here
+        $html .= $this->label();
 
-		$html .= '<input class="hidden" type="hidden" ' . $this->attributes() .' data-produrl="'. $s3['prodUrl'] .'" data-key="'. $s3['accessKey'] .'" data-folder="'. $this->entityInfo->getEntityName() . '-'. $this->field->name. '"  data-bucket="' . $s3['bucket'] . '" data-signer="' . $s3['signer'] . '" data-region="' . $s3['region'] . '" value="'. $this->value .'" />';
+        $html .= '<input class="hidden" type="hidden" ' . $this->attributes() .' data-produrl="'. $s3['prodUrl'] .'" data-key="'. $s3['accessKey'] .'" data-folder="'. strtolower($this->entityInfo->getEntityName() . '-'. $this->field->name) . '"  data-bucket="' . $s3['bucket'] . '" data-signer="' . $s3['signer'] . '" data-region="' . $s3['region'] . '" value="'. $this->value .'" />';
 
-		// DIV 1: UPLOAD BUTTON
-		$html .= '<div class="multipart-upload-container hidden">';
-			$html .= '<a class="fakeClick">' . $this->pttTrans->trans('pick_file') . '</a><input type="file" class="chooseFile" />';
-		$html .= '</div>';
-		// DIV 2: BARRA DE CARREGA
-		$html .= '<div class="load-mode hidden">';
-			$html .= '<div class="progress-bar-div col-xs-12 nopadding">';
-				$html .= '<div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"><span>0%</span></div>';
-			$html .= '</div>';
-			$html .= '<div class="action-buttons col-sm-12"><a class="btn btn-cancel">'.$this->pttTrans->trans('cancel').'</a></div>';
-		$html .= '</div>';
-		// DIV 3: RESULT
-		$html .= '<div class="result hidden">';
-			$html .= '<div class="error-div col-xs-12 nopadding">';
-				$html .= '<p class="upload-fail btn"></p>';
-			$html .= '</div>';
-			$html .= '<div class="action-buttons col-sm-12">';
-				$html .= '<a class="btn btn-retry">' . $this->pttTrans->trans('retry') . '</a>';
-			$html .= '</div>';
-		$html .= '</div>';
-		// DIV 4: FITXER, DOWNLOAD I DELETE
-		$html .= '<div class="view-mode hidden">';
-			$html .= '<input class="form-control" disabled type="text" value="'.$name.'" />';
-			$html .= '<div class="action-buttons col-sm-12">';
-				$html .= '<a class="btn btn-download" href="' . $download .'" download>' . $this->pttTrans->trans('download') . '</a>';
-				$html .= '<a class="btn btn-delete">' . $this->pttTrans->trans('delete') . '</a>';
-			$html .= '</div>';
-		$html .= '</div>';
+        // DIV 1: UPLOAD BUTTON
+        $html .= '<div class="multipart-upload-container hidden">';
+        $html .= '<a class="fakeClick">' . $this->pttTrans->trans('pick_file') . '</a><input type="file" class="chooseFile" />';
+        $html .= '</div>';
+        // DIV 2: BARRA DE CARREGA
+        $html .= '<div class="load-mode hidden">';
+        $html .= '<div class="progress-bar-div col-xs-12 nopadding">';
+        $html .= '<div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"><span>0%</span></div>';
+        $html .= '</div>';
+        $html .= '<div class="action-buttons col-sm-12"><a class="btn btn-cancel">'.$this->pttTrans->trans('cancel').'</a></div>';
+        $html .= '</div>';
+        // DIV 3: RESULT
+        $html .= '<div class="result hidden">';
+        $html .= '<div class="error-div col-xs-12 nopadding">';
+        $html .= '<p class="upload-fail btn"></p>';
+        $html .= '</div>';
+        $html .= '<div class="action-buttons col-sm-12">';
+        $html .= '<a class="btn btn-retry">' . $this->pttTrans->trans('retry') . '</a>';
+        $html .= '</div>';
+        $html .= '</div>';
+        // DIV 4: FITXER, DOWNLOAD I DELETE
+        $html .= '<div class="view-mode hidden">';
+        $html .= '<input class="form-control" disabled type="text" value="'.$name.'" />';
+        $html .= '<div class="action-buttons col-sm-12">';
+        $html .= '<a class="btn btn-download" href="' . $download .'" download>' . $this->pttTrans->trans('download') . '</a>';
+        $html .= '<a class="btn btn-delete">' . $this->pttTrans->trans('delete') . '</a>';
+        $html .= '</div>';
+        $html .= '</div>';
 
-		$html .= $this->end();
+        $html .= $this->end();
 
-		return $html;
-	}
+        return $html;
+    }
 
-	protected function extraClassesForFieldContainer()
+    protected function extraClassesForFieldContainer()
     {
         return 'form-group multipart-upload col-sm-' . $this->getWidth();
     }
